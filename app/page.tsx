51288177cbd595dcc8ea460df23dded4e568540c
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-06-18 15:28:08 +0000

    Finish home page

--- app/page.tsx
+++ app/page.tsx
@@ -153,8 +153,83 @@ export default function Home() {
       </div>
 
 
-      <div>
+      <div className="mb-32">
+        <div className="container mx-auto px-4">
+          <div className="px-48">
+            <h1 className="text-center text-4xl font-bold w-full">Nos solutions KYA-Sol</h1>
+            <div className="flex justify-center items-center my-4">
+              <div className="h-1 w-32 bg-green-300"></div>
+            </div>
+            <p className="text-center">Découvrez notre gamme complète de groupes électrosolaires adaptés à tous vos besoins énergetiques.</p>
+          </div>
+          <div className="mt-8 grid grid-cols-1 lg:grid-cols-3 gap-8">
+            {
+              [
+                ["/kya-sop-residentiel.avif", "KYA-Sol Résidentiel", "Solutions adaptées aux maisons individuelles et petits bâtiments.", ["Installation rapide", "Maintenance simplifiée", "Garantie 5 ans"], "#"],
+                ["/kya-sop-commercial.avif", "KYA-Sol Commercial", "Systèmes haute performance pour entreprises et industries.", ["Haute capacité", "Monitoring avancé", "ROI optimisé"], "#"],
+                ["/kya-sop-communautaire.avif", "KYA-Sol Communautaire", "Projets d'envergure pour collectivités et institutions.", ["Solutions sur mesure", "Financement flexible", "Impact social"], "#"]
+              ].map((value, index) => (
+                <div key={index} className="p-4 rounded-xl bg-white space-y-8">
+                  <div className="h-64">
+                    <img className="w-full h-64 object-contain" src={`${value[0]}`} alt="" />
+                  </div>
+                  <p className="font-bold text-xl">{value[1]}</p>
+                  <p className="text-sm">{value[2]}</p>
+                  <div className="space-y-4">
+                    {[...value[3]].map((_value, _index) => (
+                      <div key={_index} className="flex items-center gap-2">
+                        <p><LuCircleCheckBig color="green" /></p>
+                        <p>{_value}</p>
+                      </div>
+                    ))}
+                  </div>
+                  <Link href={`${value[4]}`}>
+                    <p className="flex items-center gap-2 justify-center text-center font-bold bg-green-600 hover:bg-green-700 transition-all hover:gap-4 duration-300 p-4 rounded-xl text-gray-100">
+                      <span>En savoir plus</span>
+                      <span><LuArrowRight color="white" /></span>
+                    </p>
+                  </Link>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </div>
 
+      <div className="mb-8">
+        <div className="container mx-auto px-4">
+          <h1 className="text-center text-4xl font-bold w-full">Contactez-nous</h1>
+          <div className="flex justify-center items-center my-4">
+            <div className="h-1 w-32 bg-green-300"></div>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+            <div className="space-y-4">
+              {/* Contact */}
+              <div>
+                <h1 className="font-bold text-green-600 underline text-xl">Adresse</h1>
+                <p>300m, rue en face du Centre Culturel Loyola (CCL),<br />Route Mission Tové, Agoè Logopé</p>
+                <p>08 BP 81101, Lomé - Togo</p>
+              </div>
+              <div>
+                <h1 className="font-bold text-green-600 underline text-xl">Horaires</h1>
+                <p>Lundi - Vendredi</p>
+                <p>07h30–12h00 | 14h00–17h30</p>
+              </div>
+              <div>
+                <h1 className="font-bold text-green-600 underline text-xl">Téléphone</h1>
+                <p>+228 70 45 34 81 / 99 99 93 80</p>
+              </div>
+              <div>
+                <h1 className="font-bold text-green-600 underline text-xl">E-mail</h1>
+                <p>info@kya-energy.com</p>
+              </div>
+            </div>
+            {/* Google maps */}
+            <div>
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3310.3053730089578!2d1.1820179740441474!3d6.241404426379205!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x10215783b880a173%3A0xe90f181e8b59c8fc!2sKYA-%C3%89nergy%20GROUP!5e1!3m2!1sfr!2stg!4v1750260157989!5m2!1sfr!2stg" className="w-full h-[300px]" allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
