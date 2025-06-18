2c0aebc8996c98266cbb0dad70175a839aa705f0
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-06-18 08:22:45 +0000

    Create header

---
+++ app/components/header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header>
+            <div>
+                <img src="/logo.avif" />
+                <p>Kya Energy Group</p>
+            </div>
+        </header>
+    );
+}
