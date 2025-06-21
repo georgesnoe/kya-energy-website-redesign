60f6742fc3b436aafe354e60d7538633cbb90fff
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-06-21 14:20:52 +0000

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Footer from "@/app/components/footer";
 import Chatbot from "@/app/components/chatbot";
 import "./globals.css";
 import { googleSansText } from "./lib/fonts";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "KYA-Energy Group",
@@ -19,10 +20,15 @@ export default function RootLayout({
     <html lang="fr">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
+        {/* Google Tag Manager */}
+        <Script src="/analytics.js" />
+        {/* End Google Tag Manager */}
       </head>
       <body
         className={`${googleSansText.className} font-medium antialiased bg-gray-100 text-gray-800 min-h-screen **:scroll-smooth **:transition-all **:duration-300 **:transition-discrete`}
       >
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P5JH3WM5"
+          height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
         <Header />
         <main>
           {children}
