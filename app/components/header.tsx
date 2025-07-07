31013218caf8c97b4a2ba8228146655da872f4d5
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-07-07 10:18:16 +0000

    Fix menu

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -3,7 +3,7 @@
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { useState } from "react";
-import { LuMenu } from "react-icons/lu";
+import { LuCross, LuMenu } from "react-icons/lu";
 
 export default function Header() {
     const pathname = usePathname();
@@ -63,7 +63,7 @@ export default function Header() {
                     {/* Bouton hamburger */}
                     <div className="xl:hidden flex items-center">
                         <button onClick={() => { setShowMenu(!showMenu); document.getElementById(pathname.slice(1))?.focus() }}>
-                            <LuMenu size={48} />
+                            {showMenu == true ? (<LuMenu size={48} />) : (<LuCross size={48} />)}
                         </button>
                     </div>
                 </div>
